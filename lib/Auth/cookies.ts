400b90493de7e2eb1ea65030c6c45622f349a4dc
Mahen <mahennekkers27@gmail.com>
2025-11-14 10:37:37 +0700

    chore: change origin server URI.

--- lib/Auth/cookies.ts
+++ lib/Auth/cookies.ts
@@ -9,6 +9,6 @@ export const GetAuthCookies = async () => {
     redirect("/dashboards");
   }
 
-  const googleLoginUrl = "http://localhost:4000/api/auth/google";
+  const googleLoginUrl = "https://uat-pract-mahennekkers27-2401-mahens-projects-240b520d.vercel.app/api/auth/google";
   return { googleLoginUrl, token };
 };
